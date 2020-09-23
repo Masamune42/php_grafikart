db47c314ec5eae971eb7a342ba54a4a79af94960
Alex <a.caignard@hotmail.fr>
2020-09-23 11:05:46 +0200

    Les classes

--- 14-class/cli/test.php
+++ 14-class/cli/test.php
@@ -1,6 +1,6 @@
 <?php
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Creneau.php';
-$creneau = new Creneau();
+$creneau = new Creneau(5,6);
 $creneau->debut = 9;
 $creneau->fin = 7;
 var_dump($creneau);
